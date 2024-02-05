42668feb9cb36628c6f3e28e0b41d6560925b733
Leonardo Côco <leonardocosouza@gmail.com>
2024-02-05 19:10:47 -0300

    Inclusão do dockerfile.

--- modules/kind/main.tf
+++ modules/kind/main.tf
@@ -1,18 +1,21 @@
 resource "kind_cluster" "cluster" {
   name = "kind"
   node_image = "kindest/node:${var.kubernetes_version}"
-
   kind_config {
     kind        = "Cluster"
     api_version = "kind.x-k8s.io/v1alpha4"
 
-  dynamic "node" {
+
+    node {
+      role = "control-plane"
+    }
+    /*dynamic "node" {
       for_each = var.control_plane
       content {
         role   = "control-plane"
         labels = node.value
       }
-    }
+    }*/
 
     dynamic "node" {
       for_each = var.nodes
@@ -28,3 +31,4 @@ data "docker_network" "kind" {
   name       = var.cluster_name
   depends_on = [kind_cluster.cluster]
 }
+
